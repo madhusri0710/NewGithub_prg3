06200cc792f34c529df3f37f5d517f97ad7a5f82
MADHU SRI J <madhusrijothimani@gmail.com>
2026-09-25 10:30:12 +0530

    Update answers.sql

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,14 @@
+CREATE TABLE Course(
+  CourseID INT PRIMARY KEY,
+  CourseName VARCHAR(30) NOT NULL,
+  Credits INT,
+  DepartmentID INT);
+  
+  INSERT INTO Course (CourseID,CourseName,Credits,DepartmentID)
+  VALUES
+  (201,'Database Systems',4,101),
+  (202,'Data Strctures',3,101),
+  (203,'Computer Networks',4,102);
+  DESCRIBE Course;
+  SELECT*FROM Course;
 
